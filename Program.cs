cc065472bcda547788c63bfd3e5e22a1daf5ff29
Shadowstrikesblu <hmoulk8@gmail.com>
2023-09-14 15:25:32 +0200

    14/09/2023 Dotnet

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System; 
+
+namespace Demo {
+    class Hello { 
+        static void Main() { 
+            Console.WriteLine("Hello, World"); 
+        }
+    }
+}
+
